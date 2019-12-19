c0eda5ecadd98af3f5e069beee35ff77ca15fc6a
luohua13950 <luohua139@qq.com>
2019-12-19 22:11:18 +0800

    单独的ip库

---
+++ check_ip.py
@@ -0,0 +1,83 @@
+__author__ = 'luohua139'
+
+import asyncio, aiohttp
+import random
+import time
+from ip_pool.crawl_ip import RedisClient, Config,logger
+from ip_pool import USER_AGENT
+
+
+class CheckIpValid():
+    def __init__(self):
+        self.test_url = "http://www.baidu.com"
+        self.count = 0
+        self.batch_size = 50
+        self.db_config = Config().db_config
+        self.redis = RedisClient(**self.db_config)
+
+    def get_proxies(self,start = 0,end=-1,http_type = "http"):
+        _proxies_list = self.redis.batch(start=start,end=end)
+        if not _proxies_list:
+            logger.error("{}到{}的代理不存在，请重新输入".format(start,end))
+            return
+        def format_proxies(_proxies):
+            _http_type = _proxies.split(":")[0]
+            proxies = { _http_type : _proxies}
+            return proxies
+        proxies_list = list(map(format_proxies,_proxies_list))
+        filter_proxies_list = [v for prxy in proxies_list for k,v in prxy.items() if k == http_type]
+        return filter_proxies_list
+
+    async def get_response(self,url,proxies):
+        conn = aiohttp.TCPConnector(limit=50)
+        async with aiohttp.ClientSession(connector=conn) as session:
+            agent = random.choice(USER_AGENT)
+            headers = {"User-Agent":agent}
+            try:
+                async with session.get(url, headers=headers,timeout = 5,proxy = proxies) as resp:
+                    if resp.status == 200:
+                        self.redis.add(proxies)
+                        logger.info("{} is success to connect test url,update score!".format(proxies))
+                        self.count +=1
+            except asyncio.TimeoutError:
+                logger.warning("{} is timeout".format(proxies))
+                self.redis.decr(proxies)
+            # except aiohttp.client_exceptions.ClientProxyConnectionError as e:
+            #     logger.error("{} is {}".format(proxies,e))
+            #     self.redis.decr(proxies)
+            # except ConnectionRefusedError as e:
+            #     logger.error("{} is {}".format(proxies,e))
+            #     self.redis.decr(proxies)
+            except Exception as e:
+                self.redis.decr(proxies)
+                logger.warning("{} is {}".format(proxies,e))
+
+    @staticmethod
+    def get_str_type_proxies(prx_batch):
+        str_proxies = list(map(lambda x:list(x.values())[0],prx_batch))
+        return str_proxies
+
+    def run_loop(self):
+        prx_batch = self.get_proxies()
+        len_prx = len(prx_batch)
+        #str_type_proxies = self.get_str_type_proxies(prx_batch)
+        loop = asyncio.get_event_loop()
+        for i in range(0,len_prx,self.batch_size):
+            end = i+self.batch_size if i+self.batch_size < len_prx else -1
+            prxs = prx_batch[i:end]
+            task = [self.get_response(self.test_url,prx) for prx in prxs]
+            loop.run_until_complete(asyncio.gather(*task))
+            time.sleep(1)
+        logger.info("{} proxy live!".format(self.count))
+
+if __name__ == '__main__':
+    tiv = CheckIpValid()
+    tiv.run_loop()
+    #import requests
+    # agent = random.choice(USER_AGENT)
+    # headers = {"User-Agent":agent}
+    # # resp = requests.get(tiv.test_url,headers = headers,proxies={"http":"http://1.197.10.34:9999"})
+    # print(resp)
+    #ret = tiv.redis.score("https://171.11.32.128:9999")
+    # ret = tiv.redis.decr("https://171.11.32.128:9999")
+    # print(ret)
---
+++ crawl_ip.py
@@ -0,0 +1,255 @@
+__author__ = 'luohua139'
+import requests
+import redis
+import logging
+import configparser
+import datetime
+import time
+from lxml import etree
+from threading import Thread
+import threading
+from multiprocessing import Process
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s ： %(message)s')
+logger = logging.getLogger(__name__)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+}
+
+
+class Config():
+    def __init__(self):
+        self.cg = configparser.ConfigParser()
+        self.cg.read("config.ini")
+
+    @property
+    def db_config(self):
+        sec = self.cg._sections
+        db_info = dict(sec["db_config"])
+        return db_info
+
+
+class RedisClient():
+    MAX_SCORE = 100
+    MIN_SCORE = 0
+    DECR_SCORE = -10
+    INIT_SCORE = 100
+    LIMITED = 10000
+
+    def __init__(self, host, port, password, db, ):
+        self.redis = redis.StrictRedis(host=host, port=port, password=password, db=db, decode_responses=True)
+
+    def add(self, proxies, score=INIT_SCORE, name="proxies"):
+        ret = self.redis.zadd(name=name, mapping={proxies: score})
+        if ret == 1:
+            logger.info("{} 代理添加成功".format(proxies))
+        else:
+            if score != self.INIT_SCORE:
+                logger.info("{} 分值更新为{}".format(proxies, score))
+            else:
+                logger.info("{} 代理已存在！".format(proxies))
+        return ret
+
+    def decr(self, proxies, score=DECR_SCORE, name="proxies"):
+        """
+        ret 为当前代理的分数
+        :param proxies:
+        :param score:
+        :param name:
+        :return:
+        """
+        ret = self.redis.zincrby(name, score, proxies, )
+        if ret > 0:
+            logger.info("{} 当前分数为{},大于0".format(proxies,ret))
+        else:
+            logger.info("{} 当前分数{},小于0，移除此ip".format(proxies,ret))
+            self.remove(proxies=proxies)
+        return ret
+
+    def remove(self, proxies, name="proxies"):
+        ret = self.redis.zrem(name, proxies)
+        if ret > 0:
+            logger.info("{}移除成功".format(proxies))
+        else:
+            logger.error("{}移除失败".format(proxies))
+        return ret
+
+    def score(self, proxies, name="proxies"):
+        ret = self.redis.zscore(name=name,value=proxies)
+        return ret
+
+    def count(self, name="proxies"):
+        _count = self.redis.zcard(name=name)
+        return _count
+
+    def countByScore(self, name="proxies", mins=MIN_SCORE, maxs=MAX_SCORE):
+        ret = self.redis.zcount(name, mins, maxs)
+        return ret
+
+    def removeByScore(self, name="proxies", mins=MIN_SCORE, maxs=MAX_SCORE):
+        ret = self.redis.zremrangebyscore(name, mins, maxs)
+        if ret > 0:
+            logger.info("移除成功{}个".format(ret))
+        return ret
+
+    def batch(self, name="proxies", start=0, end=10, desc=False, withscores=False):
+        ret = self.redis.zrange(name, start, end, desc=desc, withscores=withscores)
+        return ret
+
+
+class BaseClass(type):
+    def __new__(cls, name, base, attrs):
+        attrs["cw_func"] = []
+        count = 0
+        for k, v in attrs.items():
+            # if "crawl_" in k:
+            if k.startswith("crawl_"):
+                attrs["cw_func"].append(k)
+                count += 1
+        attrs["func_count"] = count
+        return type.__new__(cls, name, base, attrs)
+
+
+class Crawl(metaclass=BaseClass):
+    def __init__(self, valid_period=20):
+        db_info = Config().db_config
+        self.rc = RedisClient(**db_info)
+        self.valid_period = valid_period
+        self.base = datetime.timedelta(days=valid_period)
+        self.sleep_sec = 30
+
+    def check_invalid_day(self, str_time):
+        try:
+            day = datetime.datetime.now()
+            dt = datetime.datetime.strptime(str_time.split()[0], "%Y-%m-%d")
+        except Exception as e:
+            logger.error("转换时间发生错误:{}".format(e))
+            return False
+        logger.info("此ip上次验证时间为{}前".format(day.date() - dt.date()))
+        return day.date() - dt.date() < self.base
+
+    @staticmethod
+    def get_page(url, page):
+        resp = requests.get(url=url.format(page), headers=headers)
+        logger.info("{}:当前爬取第{}页".format(threading.current_thread().name, page))
+        x_page = etree.HTML(resp.text)
+        x_tr = x_page.xpath("//tbody//tr")
+        if len(x_tr) < 1:
+            x_tr = x_page.xpath("//table//tr")
+        return x_tr
+
+    def add_proxies(self, ip, port, http, str_time):
+        try:
+            http = http.lower()
+            if self.check_invalid_day(str_time):
+                proxies = "{}://{}:{}".format(http, ip, port)
+                self.rc.add(proxies)
+                return True
+            else:
+                return False
+        except Exception as e:
+            logger.error("存储过程发生异常:{}".format(e))
+
+
+    def crawl_kuaidaili(self):
+        url = "https://www.kuaidaili.com/free/inha/{}/"
+        page = 1
+        while True:
+            try:
+                x_tr = self.get_page(url, page)
+                if not self._crawl_kuaidaili(x_tr):
+                    logger.info(
+                        "{}:当前ip校验时间已超过{}天,不再继续爬取后面！".format(threading.current_thread().name, self.valid_period))
+                    break
+                page += 1
+                logger.info("快代理睡眠{}秒".format(self.sleep_sec))
+                time.sleep(self.sleep_sec)
+
+            except Exception as e:
+                logger.error("快代理爬取过程发生错误：%s" % e)
+                break
+
+
+    def _crawl_kuaidaili(self, x_tr):
+        for tr in x_tr:
+            try:
+                _ip = tr.xpath("td[1]/text()")
+                ip = _ip[0] if _ip else ""
+                _port = tr.xpath("td[2]/text()")
+                port = _port[0] if _port else ""
+                _http = tr.xpath("td[4]/text()")
+                http = _http[0] if _http else ""
+                _str_time = tr.xpath("td[7]/text()")
+                str_time = _str_time[0] if _str_time else ""
+                if not all([ip, port, http]):
+                    continue
+                if not self.add_proxies(ip, port, http, str_time):
+                    return False
+            except Exception as e:
+                logger.error("快代理解析页面发生错误：%s" % e)
+                continue
+        return True
+
+    def crawl_xici(self):
+        url = "https://www.xicidaili.com/nn/{}"
+        page = 1
+        while True:
+            try:
+                x_tr = self.get_page(url, page)
+                if not self._crawl_xici(x_tr):
+                    logger.info(
+                        "{}:当前ip校验时间已超过{}天,不再继续爬取后面！".format(threading.current_thread().name, self.valid_period))
+                    break
+                page += 1
+                logger.info("西刺睡眠{}秒".format(self.sleep_sec))
+                time.sleep(self.sleep_sec)
+            except Exception as e:
+                logger.error("西刺代理爬取过程发生错误：%s" % e)
+                break
+
+    def _crawl_xici(self, x_tr):
+        for tr in x_tr[1:]:
+            try:
+                _ip = tr.xpath("td[2]/text()")
+                ip = _ip[0] if _ip else ""
+                _port = tr.xpath("td[3]/text()")
+                port = _port[0] if _port else ""
+                _http = tr.xpath("td[6]/text()")
+                http = _http[0] if _http else ""
+                _str_time = tr.xpath("td[10]/text()")
+                str_time = _str_time[0] if _str_time else ""
+                if not all([ip, port, http]):
+                    continue
+                str_time = "20" + str_time
+                if not self.add_proxies(ip, port, http, str_time):
+                    logger.info("当前ip验证时间为{}".format(str_time))
+                    return False
+
+            except Exception as e:
+                logger.error("西刺代理解析页面发生错误：%s" % e)
+                continue
+        return True
+
+    def run(self):
+        # th_kuaidali = Thread(target=self.crawl_kuaidaili,args=())
+        th_list = []
+        for cw in self.cw_func:
+            fn = eval("self.{}".format(cw))
+            th_list.append(Thread(target=fn, args=(), name="thread-{}".format(cw)))
+            logger.info("创建线程...")
+        for th in th_list:
+            logger.info("启动线程:{}...".format(th.name))
+            th.start()
+
+
+class CheckValid():
+    def __init__(self):
+        self.test_url = "http://www.baidu.com"
+
+
+if __name__ == '__main__':
+    cw = Crawl()
+    cw.run()
+
